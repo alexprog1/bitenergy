ca15dcbe77aa4243535bb0c5bda1e27cd9360391
Alex Carvalho <alexrn17@gmail.com>
2023-03-03 16:46:22 -0300

    Conteúdo do Site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from './componentes/Header/Header';
 import Footer from './componentes/Footer/Footer';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+        <br/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/yUJv2RccUL0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br/>
+        <h1>Seremos o maior condomínio de energia solar do RN</h1>
+        <br/>
       <Footer />
-      <h1>BitEnergy</h1>
     </div>
   );
 }
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -3,17 +3,21 @@ import "./Header.css";
 
 const Header = () => {
     return(
-        <div className="header">
-            <h1>
-            <img src="/imagens/bitenergy.png" alt="Bit Energy"/>
-            </h1>
-            <ul>
-                <li>Quem Somos</li>
-                <li>Soluções</li>
-                <li>Como Funciona</li>
-                <li>Blog</li>
-                <li>Contato</li>
+        <div className="div-header">
+            <header className="header">
+            <img src="/imagens/bitenergy.png" width={"20%"} height={"20%"} alt="Bit Energy"/>
+        
+            <ul className="menu">
+                <li><a href="/">Quem somos</a></li>
+                <li><a href="/">Soluções</a></li>
+                <li><a href="/">Como funciona</a></li>
+                <li><a href="/">Blog</a></li>
+                <li><a href="/">Contato</a></li>
+                <li>
+                <button className="btn">SIMULADOR</button>
+                </li>
             </ul>
+            </header>
         </div>
     )
 }
