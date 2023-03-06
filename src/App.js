3031cfd007cb1a079a16af7db393878e1afe8611
Alex Carvalho <alexrn17@gmail.com>
2023-03-06 17:11:53 -0300

    Criação de Cards

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <main>
       <Header />
         <br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/yUJv2RccUL0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/yUJv2RccUL0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
         <br/>
-        <h1>Seremos o maior condomínio de energia solar do RN</h1>
+        <section>
+          <h1>Seremos o maior condomínio de energia solar do RN</h1>
+          <p>Gerador de energia limpa e sustentável</p>
+        </section>
         <br/>
+        <div class="row">
+          <div class="card green">
+            <h3>Especialista em energia solar</h3>
+            <p>Totalmente especializada e segura.</p>
+          </div>
+          <img className='card green' src='./imagens/card01.jpg' alt='Card'/>
+        </div>
       <Footer />
-    </div>
+    </main>
   );
 }
 
